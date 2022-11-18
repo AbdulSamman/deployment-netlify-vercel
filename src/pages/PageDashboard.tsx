9f8a31bb061e2e85d95afcba39d35fc46a3b4e27
Your Name <you@example.com>
2022-11-18 15:04:05 +0100

    added jobsImage

--- src/pages/PageDashboard.tsx
+++ src/pages/PageDashboard.tsx
@@ -3,7 +3,10 @@ export const PageDashboard = () => {
   return (
     <div className="page pageDashboard">
       <p>This is the Dashboard page.</p>
-      <img className="employment" src={employment} alt="" />
+      <img className="employment" src={employment} />
+      <div className="jobsImg">
+        <img src="images/jobs.jpg" />
+      </div>
     </div>
   );
 };
