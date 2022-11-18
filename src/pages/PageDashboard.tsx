20a55212dd33db3bacd407e3b723a1a2b0648359
Your Name <you@example.com>
2022-11-18 14:57:23 +0100

    employment.jpg

--- src/pages/PageDashboard.tsx
+++ src/pages/PageDashboard.tsx
@@ -1,7 +1,9 @@
+import employment from "../assets/Employment.jpg";
 export const PageDashboard = () => {
-	return (
-		<div className="page pageDashboard">
-			<p>This is the Dashboard page.</p>
-		</div>
-	);
+  return (
+    <div className="page pageDashboard">
+      <p>This is the Dashboard page.</p>
+      <img className="employment" src={employment} alt="" />
+    </div>
+  );
 };
