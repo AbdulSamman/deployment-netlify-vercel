84dc52af75e20d16450ca7d9c795200dd17f34ab
Your Name <you@example.com>
2023-02-19 22:04:44 +0100

    finished

--- src/pages/PageDashboard.tsx
+++ src/pages/PageDashboard.tsx
@@ -4,11 +4,16 @@ export const PageDashboard = () => {
     <div className="page pageDashboard">
       <p>This is the Dashboard page.</p>
       <div className="images">
+        {/* verschiede sources um zu sehen bei saveImage verschiedene name rauskommen */}
+        {/* richtig employment */}
         <div className="employment">
           <img src={employment} />
         </div>
         <div className="jobsImg">
           <img src="images/jobs.jpg" />
+          {/* ist falsch findet netlify net
+		  <img src="../../src/images/jobs.jpg" /> 
+		  */}
         </div>
       </div>
     </div>
--- src/pages/PageJobs.tsx
+++ src/pages/PageJobs.tsx
@@ -1,7 +1,26 @@
+import jobSources from "../data/jobSources.json";
+
 export const PageJobs = () => {
-	return (
-		<div className="page pageJobs">
-			<p>This is the Jobs page.</p>
-		</div>
-	);
+  return (
+    <div className="page pageJobs">
+      <h2>Job Sources</h2>
+      <div className="jobSources">
+        {jobSources.map((jobSource) => {
+          return (
+            <div className="jobSource" key={jobSource.id}>
+              <img src={`images/jobSources/${jobSource.idCode}.png`} />
+              <div className="info">
+                <div className="title">{jobSource.title}</div>
+                <div className="url">
+                  <a target="_blank" href={jobSource.url}>
+                    {jobSource.url}
+                  </a>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
